81ffb7091c34bbbb21530c5e394386d6d4564a8e
Piotr Bielaska <github@bielaska.pl>
2025-11-10 10:27:38 +0100

    debuging db password issues

--- hosts/rust/services/containers/dawarich/dawarich_sidekiq.nix
+++ hosts/rust/services/containers/dawarich/dawarich_sidekiq.nix
@@ -14,7 +14,8 @@
       "APPLICATION_PROTOCOL" = "http";
       "DATABASE_HOST" = "dawarich_db";
       "DATABASE_NAME" = "dawarich_development";
-      "DATABASE_PASSWORD" = "$DAWARICH_PASSWORD"; # secured with agenix
+      "DATABASE_PASSWORD" = "password";
+      #"DATABASE_PASSWORD" = "$DAWARICH_PASSWORD"; # secured with agenix
       "DATABASE_USERNAME" = "postgres";
       "PROMETHEUS_EXPORTER_ENABLED" = "false";
       "PROMETHEUS_EXPORTER_HOST" = "dawarich";
