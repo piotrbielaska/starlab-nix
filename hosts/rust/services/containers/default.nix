c16a6600d53aa0da3eb832fb5408ccfde934679a
Piotr Bielaska <github@bielaska.pl>
2025-11-10 16:18:23 +0100

    re-enabled dawarich container

--- hosts/rust/services/containers/default.nix
+++ hosts/rust/services/containers/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
     ./airtrail # save your flights history
-  #  ./dawarich # google maps location self-hosted alternative
+    ./dawarich # google maps location self-hosted alternative
   ];
 }
