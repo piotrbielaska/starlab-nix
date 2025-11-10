296b5f165db4d90172642af1b4d78a636ad741fb
Piotr Bielaska <github@bielaska.pl>
2025-11-10 09:56:43 +0100

    commented out incorrect color specification

--- home/features/cli/fzf.nix
+++ home/features/cli/fzf.nix
@@ -20,9 +20,9 @@ with lib; let
           "fg" = "#f8f8f2";
           "bg" = "#282a36"; 
           "hl" = "#bd93f9";
-          "fg+ " = "#f8f8f2";
+          #"fg+ " = "#f8f8f2";
           #"bg+ " = "#44475a";
-          "hl+ " = "#bd93f9";
+          #"hl+ " = "#bd93f9";
           "info" = "#ffb86c";
           "prompt" = "#50fa7b";
           "pointer" = "#ff79c6";
